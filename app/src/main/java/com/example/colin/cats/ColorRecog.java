1f571f01a621972c3169ed339998d21d432f413f
Glen Peek <glen.peek@gmail.com>
2015-09-27 17:00:04 +1300

    initial commit

---
+++ app/src/main/java/com/example/colin/cats/ColorRecog.java
@@ -0,0 +1,38 @@
+package com.example.colin.cats;
+
+import android.graphics.Bitmap;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ColorRecog {
+
+	HashMap<Double, Integer> concentrations = new HashMap<Double, Integer>();
+
+
+	public ColorRecog(HashMap<Double, Integer> concentrations ){
+		this.concentrations = concentrations;
+	}
+
+	private Map<Double, Double> estimateFromImage(HSBColor image) {
+		Map<Double, Double> distances = new HashMap<Double, Double>();
+		for(Map.Entry<Double, Integer> nameColours : concentrations.entrySet()){
+			Double name = nameColours.getKey();
+			int rgbColor = nameColours.getValue();
+			HSBColor hsbColor = new HSBColor(rgbColor);
+			HSBColor diff = image.differenceFrom(hsbColor);
+			double dist = Math.sqrt(diff.h * diff.h + diff.s * diff.s + diff.b * diff.b);
+			distances.put(name, dist);
+		}
+		return distances;
+	}
+
+	public Map<Double, Double> processImage(Bitmap img){
+
+		HSBImage hsbSubImage = new HSBImage(img);
+		HSBColor median = hsbSubImage.medianColor();
+		
+		return estimateFromImage(median);
+	}
+}
